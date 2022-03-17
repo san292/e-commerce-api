be623d23a9f81c65f9d4e78c820cd826258370a2
san man <sance1921@gmail.com>
2022-03-17 16:54:40 +0100

    login method

--- app/router/auth.js
+++ app/router/auth.js
@@ -1,16 +1,49 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 // REGISTER;
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString()
   });
   try {
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// LOGIN
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username
+    });
+
+    !user && res.status(401).json('Utilisateur non trouvé');
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+    const inputPassword = req.body.password;
+
+    originalPassword !== inputPassword &&
+      res.status(401).json('mot de passe erroné');
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
   } catch (error) {
     res.status(500).json(error);
   }
