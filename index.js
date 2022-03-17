92c1dda32f366c3fb396bbd96e963c329247bf04
san man <sance1921@gmail.com>
2022-03-17 09:50:43 +0100

    run server and initialize mongoose

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const PORT = process.env.PORT || 3000;
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('DB connection successfull!'))
+  .catch((error) => {
+    console.error(error);
+  });
 
 app.listen(PORT, () => {
   console.log(`backend server is running in localhost ${PORT}`);
