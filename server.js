571b431f5f9ade7c7d6455bbe8c94a863aa51139
san man <sance1921@gmail.com>
2022-05-09 10:11:58 +0200

    delete .gitsubmodule

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
 app.use('/api/checkout', stripeRoute);
-app.use(express.static(path.join(__dirname, '/ecommerce/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/ecommerce/build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, '/ecommerce/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/ecommerce/build', 'index.html'));
+// });
 
 app.use(errorMiddleware.errorHandler);
 
